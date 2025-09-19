b4ea73793facaf2ab6d6d88040f7c4cf3bcd7eda
ChairGamertag87 <76748554+ChairGamertag87@users.noreply.github.com>
2025-09-19 14:47:26 +0200

    Add First part of TP4

---
+++ src/dir/Sapins.java
@@ -0,0 +1,52 @@
+package dir;
+
+public class Sapins
+{
+    public static String genererStringLigne (int nb, char car){
+        String resultat = "";
+        for (int i = 0; i < nb; i++) {
+            resultat += car;
+        }
+        return resultat;
+    }
+
+    public static String genererSapinPlein (int hauteur){
+        String resultat = "";
+        for (int i = 0; i < hauteur; i++) {
+            resultat += genererStringLigne(hauteur - i - 1, ' ');
+            resultat += genererStringLigne(2 * i + 1, '*');
+            resultat += "\n";
+        }
+        return resultat;
+    }
+
+    public static String genererSapinVide (int hauteur){
+        String resultat = "";
+        for (int i = 0; i < hauteur; i++) {
+            resultat += genererStringLigne(hauteur - i - 1, ' ');
+            if (i == 0) {
+                resultat += "*";
+            } else if (i == hauteur - 1) {
+                resultat += genererStringLigne(2 * i + 1, '*');
+            } else {
+                resultat += "*";
+                resultat += genererStringLigne(2 * i - 1, ' ');
+                resultat += "*";
+            }
+            resultat += "\n";
+        }
+        return resultat;
+    }
+
+    public static String genererSapinCouche (int hauteur){
+        String resultat = "";
+        for (int i = 1; i <= hauteur; i++) {
+            resultat += genererStringLigne(i, '*') + "\n";
+        }
+        for (int i = hauteur - 1; i >= 1; i--) {
+            resultat += genererStringLigne(i, '*') + "\n";
+        }
+        return resultat;
+    }
+}
+
