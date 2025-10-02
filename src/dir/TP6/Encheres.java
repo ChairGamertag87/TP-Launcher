86fc537b6fa0c93960d8b1d6db7cecb1ae23e019
ChairGamertag87 <76748554+ChairGamertag87@users.noreply.github.com>
2025-10-02 11:59:25 +0200

    ✨Add TP6

---
+++ src/dir/TP6/Encheres.java
@@ -0,0 +1,74 @@
+package dir.TP6;
+
+import java.util.Scanner;
+
+public class Encheres {
+
+    /**
+     * Programme principal des enchères
+     */
+    public static void main(String[] args) {
+        Scanner clavier = new Scanner(System.in);
+
+        /* --- Version 1 : 6 enchères croissantes --- */
+        double[] enchere1 = new double[6];
+        for (int i = 0; i < 6; i++) {
+            System.out.println("Donnez une enchère : ");
+            double valeur = clavier.nextDouble();
+            while (i > 0 && valeur <= enchere1[i - 1]) {
+                System.out.println("Enchère trop faible, recommencez : ");
+                valeur = clavier.nextDouble();
+            }
+            enchere1[i] = valeur;
+        }
+        System.out.println("Enchères acceptées : ");
+        for (int i = 0; i < 6; i++) {
+            System.out.println(enchere1[i]);
+        }
+
+        /* --- Version 2 : enchère > somme des précédentes --- */
+        double[] enchere2 = new double[6];
+        double somme = 0;
+        for (int i = 0; i < 6; i++) {
+            System.out.println("Donnez une enchère : ");
+            double valeur = clavier.nextDouble();
+            while (valeur <= somme) {
+                System.out.println("Enchère trop faible, recommencez : ");
+                valeur = clavier.nextDouble();
+            }
+            enchere2[i] = valeur;
+            somme += valeur;
+        }
+        System.out.println("Enchères acceptées : ");
+        for (int i = 0; i < 6; i++) {
+            System.out.println(enchere2[i]);
+        }
+
+        /* --- Version 3 : possibilité d’arrêter avant 6 enchères --- */
+        double[] enchere3 = new double[6];
+        int nb = 0;
+        somme = 0;
+        boolean continuer = true;
+        while (nb < 6 && continuer) {
+            System.out.println("Quelqu’un veut-il enchérir ? (o/n) : ");
+            String rep = clavier.next();
+            if (rep.equals("o")) {
+                System.out.println("Donnez une enchère : ");
+                double valeur = clavier.nextDouble();
+                while (valeur <= somme) {
+                    System.out.println("Enchère trop faible, recommencez : ");
+                    valeur = clavier.nextDouble();
+                }
+                enchere3[nb] = valeur;
+                somme += valeur;
+                nb++;
+            } else {
+                continuer = false;
+            }
+        }
+        System.out.println("Enchères acceptées : ");
+        for (int i = 0; i < nb; i++) {
+            System.out.println(enchere3[i]);
+        }
+    }
+}
---
+++ src/dir/TP6/Sauts.java
@@ -0,0 +1,104 @@
+package dir.TP6;
+
+import java.util.Scanner;
+
+public class Sauts {
+
+    public static int saisieIntMinMax(int pfMin, int pfMax) {
+        int saisie;
+        Scanner clavier = new Scanner(System.in);
+
+        System.out.println("Donnez une valeur comprise entre " + pfMin + " et " + pfMax + " :");
+        saisie = clavier.nextInt();
+        while (saisie < pfMin || saisie > pfMax) {
+            System.out.println("Donnez une valeur comprise entre " + pfMin + " et " + pfMax + " :");
+            saisie = clavier.nextInt();
+        }
+        return saisie;
+    }
+
+    public static int saisieIntMin(int pfMin) {
+        int saisie;
+        Scanner clavier = new Scanner(System.in);
+        System.out.println("Donnez une valeur au moins égale à " + pfMin + " :");
+        saisie = clavier.nextInt();
+        while (saisie < pfMin) {
+            System.out.println("Donnez une valeur au moins égale à " + pfMin + " :");
+            saisie = clavier.nextInt();
+        }
+        return saisie;
+    }
+
+    public static double saisieDoubleMin(double pfMin) {
+        double saisie;
+        Scanner clavier = new Scanner(System.in);
+        System.out.println("Donnez une valeur au moins égale à " + pfMin + " :");
+        saisie = clavier.nextDouble();
+        while (saisie < pfMin) {
+            System.out.println("Donnez une valeur au moins égale à " + pfMin + " :");
+            saisie = clavier.nextDouble();
+        }
+        return saisie;
+    }
+
+    public static void saisieTabD(int pfNbSaisies, double pfTab[]) {
+        int i;
+        for (i = 0; i < pfNbSaisies; i++) {
+            System.out.println("Valeur n°" + (i + 1) + " :");
+            pfTab[i] = saisieDoubleMin(0);
+        }
+    }
+
+    public static double moyenneSauts(int pfNbSauts, double pfTab[]) {
+        if (pfNbSauts == 0) {
+            return 0.0;
+        }
+        double somme = 0.0;
+        for (int i = 0; i < pfNbSauts; i++) {
+            somme += pfTab[i];
+        }
+        return somme / pfNbSauts;
+    }
+
+    public static double meilleurSaut(int pfNbSauts, double pfTab[]) {
+        double max = pfTab[0];
+        for (int i = 1; i < pfNbSauts; i++) {
+            if (pfTab[i] > max) {
+                max = pfTab[i];
+            }
+        }
+        return max;
+    }
+
+    public static double pireSaut(int pfNbSauts, double pfTab[]) {
+        double min = pfTab[0];
+        for (int i = 1; i < pfNbSauts; i++) {
+            if (pfTab[i] < min) {
+                min = pfTab[i];
+            }
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+
+        /* --- Saisie des sauts --- */
+        int nbSauts = saisieIntMinMax(0, 15);
+        double[] tab = new double[nbSauts];
+        saisieTabD(nbSauts, tab);
+
+        /* --- Moyenne des sauts --- */
+        double moyenne = moyenneSauts(nbSauts, tab);
+        System.out.println("Moyenne des sauts : " + moyenne);
+
+        /* --- Meilleur saut --- */
+        if (nbSauts > 0) {
+            double meilleur = meilleurSaut(nbSauts, tab);
+            System.out.println("Meilleur saut : " + meilleur);
+
+            /* --- Pire saut --- */
+            double pire = pireSaut(nbSauts, tab);
+            System.out.println("Pire saut : " + pire);
+        }
+    }
+}
